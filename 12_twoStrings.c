21c6b8ccadcdc2255214711d1da50b0a64eccb4c
roy1paz <roy1paz@gmail.com>
2019-08-04 00:42:40 +0300

    Basic c with Roy the hacker

--- 12_twoStrings.c
+++ 12_twoStrings.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
@@ -22,17 +23,25 @@ int main() {
 		
 		for(i = 0; str1[i] != '\0'; i++){ 
 
-			if (str1[i] == str2[j]) { 
-				j++;
+			if (str1[i] == str2[j]) { // First, check for the first letter of str2 in str1, and then move to the next letters.
+				j++; // Increase j when found a letter.
 			}
 
 			else {
-				j = 0;
+				j = 0; // Reset to 0, When there is no match.
 			}
 
-			if (str2[j] == '\0') {
+			if (str2[j] == '\0') { // Reached to \0, all letters of str2 fits in str1.
 					printf("True\n");
-				}
+					break;
+			}
+
+			if (strlen(str2) > strlen(str1) - i) { // In case that the letter number of str1 is smaller then str2  
+				printf("False\n");
+				break;
+			}
+			
+
 		}
 
 		return 0;
