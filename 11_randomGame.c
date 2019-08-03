f84aed4b683bb48f1f1602b7946c03112f0a4431
roy1paz <roy1paz@gmail.com>
2019-08-03 08:26:57 +0300

    Basic c with Roy the hacker

---
+++ 11_randomGame.c
@@ -0,0 +1,31 @@
+/**
+ * Author: Roy Paz
+ *
+ * Game of input and random output.
+ *
+ * Input: integer between 1 to 3.
+ * Output: win if the random output is equal to the input. 
+ */
+
+#include <time.h>
+#include <stdlib.h>
+
+ int main() {
+
+ 	time_t t;
+ 	int num;
+ 	srand((unsigned) time(&t));
+
+ 	printf("Enter a number between 1 to 3: ");
+ 	scanf("%d", &num);
+	if (num == rand() % 4) {
+		printf("You win!\n");
+	}
+
+	else {
+
+		printf("You lose!\n");
+	}
+
+	return 0;
+}
