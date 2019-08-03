8b76e058029b7e5f92515fab694e8bb920321468
roy1paz <roy1paz@gmail.com>
2019-08-03 23:40:22 +0300

    Basic c with Roy the hacker

--- 12_twoStrings.c
+++ 12_twoStrings.c
@@ -1,43 +1,39 @@
+/**
+ * Author: Roy Paz
+ *
+ * Algorithm that chack if the second string fits to the first string .
+ *
+ * Input: two string.
+ * Output: True if the second string fits.
+ */
+
 #include <stdio.h>
 
 int main() {
 
 		char str1[50];
 		char str2[50];
-		int i, j;
-
+		int i, j = 0;
 
 		printf("Please enter a sentence: ");
-		scanf("%c", str1);
+		gets(str1);
 		printf("Please enter a new sentence: ");
-		scanf("%c", str2);
-
+		gets(str2);
 		
-		for(i = 0; str1[i] = '\0'; i++){
+		for(i = 0; str1[i] != '\0'; i++){ 
 
-			do{
+			if (str1[i] == str2[j]) { 
+				j++;
+			}
 
-				if (str1[i] == str2[0]){ // try to find the first letter of str2 in str1
+			else {
+				j = 0;
+			}
 
-					for(j = 1; str2[j] = '\0'; j++){ 
-
-						do {							
-
-							if (str2[j] == str1[i + 1]) { // try to find if the other letters fit. 
-
-
-							}
-
-							printf("True\n");
-
-						}while (str2[j] == str1[i + 1]); // if fail, go out and try to find again the first letter.
-					}
-				}	
-			} while (str1[i] == str2[0]);	// if fail, go out and print false.
+			if (str2[j] == '\0') {
+					printf("True\n");
+				}
 		}
-			
-		printf("False\n");
-		
 
 		return 0;
-	}	
+}	
