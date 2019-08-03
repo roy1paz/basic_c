4ba10a7cb8630d5dc9ae38ac59ec59c908d47a4a
roy1paz <roy1paz@gmail.com>
2019-08-03 13:30:50 +0300

    Bacis c with Roy the hacker

--- 11_randomGame.c
+++ 11_randomGame.c
@@ -12,20 +12,52 @@
 
  int main() {
 
+ 	char again;
  	time_t t;
  	int num;
  	srand((unsigned) time(&t));
+ 	do {
 
- 	printf("Enter a number between 1 to 3: ");
- 	scanf("%d", &num);
-	if (num == 1 + rand() % 3) {
-		printf("You win!\n");
-	}
+ 		printf("Enter a number between 1 to 3: ");
+ 		scanf("%d", &num);
+ 		if (num > 3 || num < 1) {
 
-	else {
+ 			printf("Wrong number\n");
+ 		}
 
-		printf("You lose!\n");
-	}
+		else if (num == 1 + rand() % 3) {
+
+			printf("You win!\n");
+		}	
+
+		else {
+
+			printf("You lose!\n");
+		}
+		do {
+	  	printf("Play again? (y/n): ");
+    	scanf("%c", &again);
+   		}while (again != 'y' && again != 'Y' && again != 'n' && again != 'N'); // The user can enter only: n, N, Y, y.
+
+    }while ((again == 'y') || (again == 'Y')); // Run the code again.
+      
+   	printf("Arrivederci!\n"); // In case that the user choose 'n' 'N'.
 
 	return 0;
 }
+
+
+
+
+/*
+		printf("Play again? (y/n): \n");
+      	scanf("%c", &again);
+      	/* if (again == 'n' || again == 'N') {
+
+      		printf("Arrivederci\n");
+      	}
+
+      	else {
+
+      		printf("Try again.\n");
+      	*/
